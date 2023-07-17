b132455124c419e84ceab2882966d27854a4e950
Jackie <jargalan0713@gmail.com>
2023-07-17 14:40:15 -0500

    Initial Commit

---
+++ sustainabilibee/sustainabilibeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  sustainabilibeeApp.swift
+//  sustainabilibee
+//
+//  Created by Scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct sustainabilibeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
