2ed022cc5548e0aa48bd846ff88bba1cc2e673b1
Jackie <jargalan0713@gmail.com>
2023-07-18 13:04:38 -0500

    commit1

--- sustainabilibee/ContentView.swift
+++ sustainabilibee/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var yellowTheme : Color = Color(red: 255/255, green: 213/250, blue: 80/250)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 50.0){
+                Spacer().frame(height: 50)
+                VStack{
+                    Image("logo")
+                    Text("sustainabilibee")
+                        .font(.custom("AlegreyaSansSC-Regular", size: 32))
+                        .fontWeight(.semibold)
+                }
+                
+                NavigationLink(destination: Text("Goal Tracker")) {
+                    Text("Goal Tracker")
+                        .font(.custom("AlegreyaSansSC-Regular", size: 32))
+                        .padding(EdgeInsets(top: 10, leading: 80, bottom: 10, trailing: 80))
+                        .foregroundColor(Color.black)
+                        .background(yellowTheme)
+                        .cornerRadius(30)
+                        
+                }
+                NavigationLink(destination: Text("Tips")) {
+                    Text("Sustainability tips")
+                        .font(.largeTitle)
+                        .padding(EdgeInsets(top: 10, leading: 45, bottom: 10, trailing: 45))
+                        .foregroundColor(Color.black)
+                        .background(yellowTheme)
+                        .cornerRadius(30)
+
+                }
+                NavigationLink(destination: Text("Our Mission")) {
+                    Text("Our Mission")
+                        .font(.largeTitle)
+                        .padding(EdgeInsets(top: 10, leading: 85, bottom: 10, trailing: 85))
+                        .foregroundColor(Color.black)
+                        .background(yellowTheme)
+                        .cornerRadius(30)
+
+                }
+                NavigationLink(destination: Text("More resources")) {
+                    Text("More resources")
+                        .font(.largeTitle)
+                        .padding(EdgeInsets(top: 10, leading: 60, bottom: 10, trailing: 60))
+                        .foregroundColor(Color.black)
+                        .background(yellowTheme)
+                        .cornerRadius(30)
+
+                }
+                Spacer().frame(height: 80)
+            }
         }
-        .padding()
+        
     }
 }
 
