5edf72716db6a27f1a9a1bd2dc263d9207b55205
Katie <leekatie2327@gmail.com>
2023-07-19 11:20:31 -0500

    added our mission

--- sustainabilibee/ourmission.swift
+++ sustainabilibee/ourmission.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct ourmission: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            HStack(spacing:20) {
+                Text("Our Mission")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+               
+                Image("target").resizable(resizingMode: .stretch).aspectRatio(contentMode: .fit).frame(width: 50.0)
+
+            }
+            
+            Spacer().frame(height:30)
+            
+            Text("The efforts of 1 single bee may not be world-changing, but thousands of bees together can support a whole ecosystem. However, without each individual bee, it’s impossible to make anything happen.")
+                .font(.title3)
+            
+            Spacer().frame(height:30)
+            
+            Image("logo")
+            
+        }.padding(50)
     }
 }
 
